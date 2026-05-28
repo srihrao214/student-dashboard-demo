2ba53e9e25a108401c546a91e9e3f01dd7d4231e
srihr <srihrao@gmail.com>
2026-05-28 23:48:49 +0200

    Add data loading test

---
+++ src/test/java/com/example/student_dashboard/StudentDashboardDataTest.java
@@ -0,0 +1,35 @@
+package com.example.student_dashboard;
+
+import com.example.student_dashboard.repository.StudentRepository;
+import com.example.student_dashboard.repository.TeacherRepository;
+import com.example.student_dashboard.repository.LectureRepository;
+import com.example.student_dashboard.repository.ExamRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class StudentDashboardDataTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private LectureRepository lectureRepository;
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    @Test
+    void shouldLoadInitialDemoData() {
+        assertEquals(10, studentRepository.count());
+        assertEquals(10, teacherRepository.count());
+        assertEquals(10, lectureRepository.count());
+        assertEquals(10, examRepository.count());
+    }
+}
