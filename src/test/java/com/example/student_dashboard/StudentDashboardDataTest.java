609aedf1dc5a7a52e0e9bdbe4d60c4e5c404c14e
srihr <srihrao@gmail.com>
2026-05-30 00:48:59 +0200

    Disable demo data test

--- src/test/java/com/example/student_dashboard/StudentDashboardDataTest.java
+++ src/test/java/com/example/student_dashboard/StudentDashboardDataTest.java
@@ -4,12 +4,14 @@ import com.example.student_dashboard.repository.StudentRepository;
 import com.example.student_dashboard.repository.TeacherRepository;
 import com.example.student_dashboard.repository.LectureRepository;
 import com.example.student_dashboard.repository.ExamRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Disabled
 @SpringBootTest
 class StudentDashboardDataTest {
 
